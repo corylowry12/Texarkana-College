c95d99688e31aea6657eb59d3c74c317bba93f4f
corylowry12 <corylowry12@gmail.com>
2021-03-02 12:33:58 -0600

    First Commit

---
+++ app/src/main/java/com/cory/texarkanacollege/FirebaseMessaging.kt
@@ -0,0 +1,38 @@
+package com.cory.texarkanacollege
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class FirebaseMessaging : FirebaseMessagingService() {
+
+    override fun onDeletedMessages() {
+        super.onDeletedMessages()
+        Log.d("Deleted", "Deleted Message")
+    }
+
+    override fun onMessageReceived(p0: RemoteMessage) {
+        super.onMessageReceived(p0)
+        Log.d("Message", "From: ${p0.from}")
+
+        if (p0.data.isNotEmpty()) {
+            Log.d("Message", "Message data payload: ${p0.data}")
+
+        }
+        p0.notification?.let {
+            Log.d("Message", "Message Notification Body: ${it.body}")
+        }
+    }
+
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
+        Log.d("Token", "Refreshed token: $p0")
+
+        sendRegistrationToServer(p0)
+    }
+
+    private fun sendRegistrationToServer(p0: String?) {
+        Log.d("token", "sendRegistrationTokenToServer($p0)")
+    }
+
+}
---
+++ app/src/main/java/com/cory/texarkanacollege/MainActivity.kt
@@ -0,0 +1,182 @@
+package com.cory.texarkanacollege
+
+import android.annotation.SuppressLint
+import android.app.DownloadManager
+import android.content.Context
+import android.net.Uri
+import android.os.*
+import android.util.Log
+import android.webkit.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.google.android.gms.ads.*
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.messaging.FirebaseMessaging
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+    private lateinit var refreshLayout: SwipeRefreshLayout
+    private var isLoaded: Boolean = false
+    private val permissionRequestCode = 1
+    private lateinit var managePermissions: ManagePermissions
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Sample AdMob app ID: ca-app-pub-3940256099942544~3347511713
+        MobileAds.initialize(this)
+        val adView = AdView(this)
+        adView.adSize = AdSize.BANNER
+        adView.adUnitId = "ca-app-pub-4546055219731501/9641132280"
+        val mAdView = findViewById<AdView>(R.id.adView)
+        val adRequest = AdRequest.Builder()
+            .build()
+        mAdView.loadAd(adRequest)
+        mAdView.adListener = object : AdListener() {
+
+        }
+
+        refreshLayout = findViewById(R.id.refreshLayout)
+        webView = findViewById(R.id.webView)
+
+        val list = listOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+        managePermissions = ManagePermissions(this, list, permissionRequestCode)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            managePermissions.checkPermissions(this)
+
+        loadWebview()
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if(!task.isSuccessful) {
+                Log.w("token", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            val token = task.result
+
+            Log.d("Token", token)
+        })
+
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun loadWebview() {
+        val url = "https://my.texarkanacollege.edu/ICS/"
+        webView.loadUrl(url)
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                val url2 = request?.url.toString()
+                if(url2.contains("LearningToolsPortlet")) {
+                    view?.loadUrl("javascript:$url2")
+                    return true
+                }
+                else {
+                    view?.loadUrl(url2)
+                }
+                return super.shouldOverrideUrlLoading(view, request)
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                isLoaded = true
+                if (refreshLayout.isRefreshing) {
+                    refreshLayout.isRefreshing = false
+                }
+                super.onPageFinished(view, url)
+            }
+
+            override fun onReceivedError(view: WebView?, request: WebResourceRequest?, error: WebResourceError?) {
+                isLoaded = false
+                Snackbar.make(findViewById(R.id.constraintLayout), applicationContext.getString(R.string.please_refresh), Snackbar.LENGTH_LONG)
+                    .show()
+                super.onReceivedError(view, request, error)
+            }
+        }
+
+        webView.setDownloadListener { url1, userAgent, contentDisposition, mimeType, _ ->
+            if (managePermissions.checkPermissions(this)) {
+                val request = DownloadManager.Request(Uri.parse(url1))
+                request.setMimeType(mimeType)
+                val cookies = CookieManager.getInstance().getCookie(url1)
+                request.addRequestHeader("cookie", cookies)
+                request.addRequestHeader("User-Agent", userAgent)
+                request.setDescription("Downloading file...")
+                request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimeType))
+                //request.allowScanningByMediaScanner()
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, contentDisposition, mimeType))
+                val downloadManager : DownloadManager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+                downloadManager.enqueue(request)
+                Snackbar.make(findViewById(R.id.constraintLayout), applicationContext.getString(R.string.downloading), Snackbar.LENGTH_LONG)
+                    .show()
+            } else {
+                Snackbar.make(findViewById(R.id.constraintLayout), applicationContext.getString(R.string.cant_download_permissions_not_granted), Snackbar.LENGTH_LONG)
+
+                    .show()
+            }
+        }
+
+        val settings = webView.settings
+        settings.domStorageEnabled = true
+        settings.javaScriptEnabled = true
+        settings.javaScriptCanOpenWindowsAutomatically = true
+
+        refreshLayout.setOnRefreshListener { webView.reload() }
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            permissionRequestCode -> {
+                val isPermissionGranted = managePermissions.processPermissionsResult(requestCode, permissions, grantResults)
+                if(isPermissionGranted) {
+                    Snackbar.make(findViewById(R.id.constraintLayout), applicationContext.getString(R.string.permissions_granted), Snackbar.LENGTH_LONG)
+                        .show()
+                }
+                else {
+                    Snackbar.make(findViewById(R.id.constraintLayout), applicationContext.getString(R.string.permissions_denied), Snackbar.LENGTH_LONG)
+                        .show()
+                }
+                return
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        webView.saveState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        webView.restoreState(savedInstanceState)
+    }
+
+    private var doubleBackToExitPressedOnce = false
+
+    override fun onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack()
+        }
+        else {
+            if(doubleBackToExitPressedOnce) {
+                super.onBackPressed()
+                return
+            }
+           this.doubleBackToExitPressedOnce = true
+            Snackbar.make(findViewById(R.id.constraintLayout), applicationContext.getString(R.string.please_click_back_again), Snackbar.LENGTH_LONG)
+                .show()
+
+            Looper.myLooper()?.let {
+                Handler(it).postDelayed({
+                    doubleBackToExitPressedOnce = false
+                }, 2000)
+            }
+        }
+    }
+}
